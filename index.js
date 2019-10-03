272e97485ce3028ff686a88d1ca4f6252568d077
Pratap2018 <dcat9816@gmail.com>
2019-10-03 18:28:23 +0530

    handled chrash

--- index.js
+++ index.js
@@ -21,16 +21,25 @@ client.on('message',message=>{
         message.reply(message.author.name);
 
     }
+    if(message.content== `${prefix}creator`){
+           message.channel.send("Pratap Mridha Created Me :wave:");
+           message.reply("You can follow him on facebook ,Instagram, Github");
+           message.channel.send("Facebook :"+" https://www.facebook.com/pratap.mridha.3");
+           message.channel.send("Instagram :"+" https://www.instagram.com/indiapratap2");
+           message.channel.send("Github :"+" https://github.com/Pratap2018");
+           message.channel.send("Thanks in advance :wave:");
+    }
+
     if(message.content== `${prefix}hi`){
         message.channel.send("Hi welcome, ");
         message.reply(message.author.name);
-
         message.channel.send("How can I help you ?");        
-
     }
-    if(message.content.startsWith(`${prefix}kick`)){
+    if(message.content==`${prefix}kick`){
+            message.reply("Please mention the member ");
+    }else  if(message.content.startsWith(`${prefix}kick`)){
     if(message.member.hasPermission(['KICK_MEMBERS','BAN_MEMBERS'])){
-            let member=message.mentions.members.first());
+            let member=message.mentions.members.first();
             member.kick().then(member=>{
                 message.channel.send(":wave: "+member.displayName+" has been kicked.")
             })
